753f3ad9f897486f1a0a720830f2be8279088c10
TWcamel <j0930495159@gmail.com>
2020-07-08 16:28:39 +0800

    front-end pages

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="homePage">
+    <h1>This is full capcha's Demo</h1>
+    <div class="output">
+      Output
+      <div class="tune-number">
+        Tune Number:
+        <i>{{ tuneNumber }}</i>
+      </div>
+      <div class="classes">
+        Classes:
+        <i>{{ classes }}</i>
+      </div>
+      <div class="abcelem">
+        Struct returned from callback:
+        <br />
+        <i>{{ elem }}</i>
+      </div>
+    </div>
+    <p class="instructions">
+      Click around on the various parts of the sheet music and see what the
+      resultant output is.
+    </p>
+    <div id="paper"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {}
+};
+</script>
+
+<style></style>
