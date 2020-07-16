3e539336f2f4bd8539c58a2b88d5eca3c9a4e9df
楊宗翰 <feveraly@gmail.com>
2020-07-16 17:17:43 +0800

    add linkDetail

---
+++ front-end/src/components/LinkDetail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div style="margin:0 auto; padding:1rem 1.5rem">
+        <b-input-group prepend="Short URL" style="place-content: center;">
+        <b-form-input type="text" v-model="shortLink" style="max-width:400px"/>  
+            <b-input-group-append>
+                <b-button variant="outline-primary" @click.prevent="copyFunction"> <b-icon-clipboard/> </b-button>
+            </b-input-group-append>
+        </b-input-group>
+        <div>Long URL: {{ longLink }} </div>
+        <div>Short URL: {{ shortLink }} </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["longLink", "shortLink"],
+    methods: {
+    copyFunction() {
+        alert("This is a copy function!");
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
--- front-end/src/components/ShortLink.vue
+++ front-end/src/components/ShortLink.vue
@@ -1,11 +1,11 @@
 <template>
   <b-col cols="12" id="linkApp" class="mt-4">
-    <linkDetail
-      v-for="link in links_raw"
+    <link-detail
+      v-for="link in links"
       :key="link.id"
-      :longLinks="link.longLink"
-      :shortLinks="link.shortLink"
-    ></linkDetail>
+      :longLink="link.longLink"
+      :shortLink="link.shortLink"
+    ></link-detail>
     <!-- TODO: -->
     Track the total of clicks in real-time from your shortened URL.
     Create other shortened URL.
@@ -13,35 +13,16 @@
 </template>
 
 <script>
-import Vue from "vue";
-const linkDetail = Vue.component("showLinks", {
-  props: ["longLinks", "shortLinks"],
-  template: `
-  <div style="margin:0 auto; padding:1rem 1.5rem">
-  <b-input-group prepend="Short URL" style="place-content: center;">
-    <b-form-input type="text" v-model="shortLinks" style="max-width:400px"/>  
-      <b-input-group-append>
-        <b-button variant="outline-primary" @click.prevent="copyFunction"> <b-icon-clipboard/> </b-button>
-      </b-input-group-append>
-  </b-input-group>
-    <div>Long URL: {{ longLinks }} </div>
-    <div>Short URL: {{ shortLinks }} </div>
-  </div>
-  `,
-  methods: {
-    copyFunction() {
-      alert("This is a copy function!");
-    }
-  }
-});
+import LinkDetail from '@/components/LinkDetail.vue'
+
 export default {
   name: "linkApp",
   components: {
-    linkDetail
+    LinkDetail
   },
   data() {
     return {
-      links_raw: [
+      links: [
         { id: 1, longLink: "https://google.com", shortLink: "asdlsakjh" },
         { id: 2, longLink: "https://medium.com", shortLink: "213521234" }
       ]
