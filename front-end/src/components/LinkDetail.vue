ad1c1e7e8c9bde94eb120c05968325d36ed7da64
楊宗翰 <feveraly@gmail.com>
2020-07-16 18:24:08 +0800

    link detail refactor

--- front-end/src/components/LinkDetail.vue
+++ front-end/src/components/LinkDetail.vue
@@ -1,47 +1,68 @@
 <template>
     <b-card class="mb-2">
-        <b-input-group prepend="Short URL" style="place-content: center;" class="mb-2">
-        <b-form-input type="text" v-model="shortLink" />
-        <b-input-group-append>
-            <b-button
-            variant="outline-primary"
-            @click="copyText"
-            v-b-tooltip.hover
-            :title="messages"
-            >
-            <b-icon-clipboard />
-            </b-button>
-        </b-input-group-append>
-        </b-input-group>
-        <b-input-group prepend="Long URL" style="place-content: center;" class="mb-2">
-        <b-form-input type="text" v-model="longLink"/>
-        <b-input-group-append>
-            <b-button variant="outline-primary" @click.prevent="copyText">
-            <b-icon-clipboard />
-            </b-button>
-        </b-input-group-append>
-        </b-input-group>
-        <b-card-text>Total count of this link: {{}}</b-card-text>
+        <b-row>
+            <b-col cols="12" sm="8">
+                <b-input-group prepend="Short URL" style="place-content: center;" class="mt-1">
+                    <b-form-input type="text" v-model="link.short_link" />
+                    <b-input-group-append>
+                        <b-button
+                        variant="outline-primary"
+                        @click="copyText"
+                        v-b-tooltip.hover
+                        :title="messages"
+                        >
+                        <b-icon-clipboard />
+                        </b-button>
+                    </b-input-group-append>
+                </b-input-group>
+                <b-input-group prepend="Long URL" style="place-content: center;" class="mt-1">
+                    <b-form-input type="text" v-model="link.long_link"/>
+                    <b-input-group-append>
+                        <b-button variant="outline-primary" @click.prevent="copyText">
+                        <b-icon-clipboard />
+                        </b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-col>
+            <b-col cols="12" sm="4" style="text-align: left;">
+                <b-input-group style="place-content: center;" class="mt-1">
+                    <b-form-input type="text" :placeholder="'點擊次數: ' + link.click_number" 
+                        style="border: none; cursor: default; pointer-events: none;"/>
+                </b-input-group>
+                <b-input-group style="place-content: center;" class="mt-1">
+                    <b-form-input type="text" :placeholder="'創立時間: ' + link.register_time.toLocaleString()" 
+                    style="border: none; cursor: default; pointer-events: none;"/>
+                </b-input-group>
+            </b-col>
+        </b-row>
+
     </b-card>
 </template>
 
 <script>
 export default {
-  props: ["longLink", "shortLink", "messages"],
-  methods: {
-    copyText() {
-        this.$copyText(this.longLink).then(
-            ele => {
-                this.title = "copied!";
+    props: ["link", "messages"],
+
+    data() {
+        return {
+            // link: {}
+        }
+    },
+
+    methods: {
+        copyText() {
+            this.$copyText(this.longLink).then(
+                ele => {
+                    this.title = "copied!";
+                    console.log(ele);
+                },
+                ele => {
+                alert("Fail to copy");
                 console.log(ele);
-            },
-            ele => {
-            alert("Fail to copy");
-            console.log(ele);
-            }
-        )
+                }
+            )
+        }
     }
-  }
 };
 </script>
 
--- front-end/src/components/UserInfo.vue
+++ front-end/src/components/UserInfo.vue
@@ -2,8 +2,7 @@
     <b-row>
       <b-col cols="12" v-for="link in links" :key="link.id">
         <link-detail
-          :longLink="link.longLink"
-          :shortLink="link.shortLink"
+          :link=link
           :messages="copyStatus"
         ></link-detail>
       </b-col>
@@ -12,25 +11,40 @@
 
 <script>
 import LinkDetail from '@/components/LinkDetail.vue'
+// import linkService from '@/services/linkService.js'
 
 export default {
-  components: {
-    LinkDetail
-  },
-  data: function() {
-    return {
-      links: [
-        {
-          id: 1,
-          longLink: "https:gohaskfjhogle.com",
-          shortLink: "https:google.com"
-        },
-        { id: 2, longLink: "https:google.com", shortLink: "https:google.com" }
-      ],
-      copyStatus: "Tap to copy URL"
-    };
-  }
-};
+    components: {
+        LinkDetail
+    },
+
+    async mounted() {
+        // const links = await linkService.getLinks()
+        // this.links = links
+    },
+
+    data() {
+        return {
+          links: [
+            {
+                _id: 1,
+                long_link: "https://gohaskfjhogle.com",
+                short_link: "https://google.com",
+                click_number: 2357,
+                register_time: new Date()
+            },
+            { 
+                _id: 2,
+                long_link: "https://gohaskfjhogle.com",
+                short_link: "https://google.com",
+                click_number: 7768,
+                register_time: new Date()
+            }
+          ],
+          copyStatus: "Tap to copy URL"
+        }
+    }
+}
 </script>
 
 <style scoped>
